7384596264c4825475c05bbfbb878871d839988b
AliBarkinKara07 <111884407+AliBarkinKara07@users.noreply.github.com>
2022-10-28 15:40:41 +0300

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir Sayı Giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+
+            }
+            Console.WriteLine(toplam / sayi);
+
+            string[] arabalar = { "BMV", "FORD", "MERCEDES" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+
+            }
+        }
+    }
+}
